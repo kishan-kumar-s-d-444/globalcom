eea4902ccdbdb2380d9cb790f32c9c0e1701f24f
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2024-12-07 16:27:00 +0530

    ready for deployment

---
+++ backend/index.js
@@ -0,0 +1,48 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import postRoute from "./routes/post.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./socket/socket.js";
+import path from "path";
+dotenv.config({});
+const __dirname = path.resolve();
+
+//middlewares
+
+
+const PORT =process.env.PORT || 3000;
+app.get("/",(req,res)=>{
+    return res.status(200).json({
+        message:"I am coming from backend",
+        success:true
+    })
+})
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
+const corsOptions = {
+    origin:'http://localhost:5173',
+    credentials: true
+}
+app.use(cors(corsOptions));
+
+app.use("/api/v1/user", userRoute);
+// "http://localhost:8000/api/v1/user/register"
+app.use("/api/v1/post", postRoute);
+app.use("/api/v1/message", messageRoute);
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+})
+
+
+server.listen(PORT, () => {
+    connectDB();
+    console.log(`Server listen at port ${PORT}`);
+});
